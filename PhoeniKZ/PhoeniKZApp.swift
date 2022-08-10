45a22f1b7a9aeb1fe14830a1b3abbbccf1c37b17
Kaden Zheng <KadenZheng@mail.fresnostate.edu>
2022-08-10 12:27:03 -0700

    Initial Commit

---
+++ PhoeniKZ/PhoeniKZApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoeniKZApp.swift
+//  PhoeniKZ
+//
+//  Created by LaptopCartUser on 8/10/22.
+//
+
+import SwiftUI
+
+@main
+struct PhoeniKZApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
